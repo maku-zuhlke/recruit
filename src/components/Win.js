2b398329c58bf79cc9c01de91dbb96759f8b268b
lewa <lewa@zuhlke.com>
2016-06-30 15:56:42 +0100

    Studying how css animations work

--- src/components/Win.js
+++ src/components/Win.js
@@ -16,38 +16,35 @@ class Win extends Component {
 
   render() {
     return (
-      <div>
-        <ScaleModal ref="modal">
-          <div className="sp-container"><div className="sp-content">
-          <div className="sp-row">
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-          </div>
-          <div className="sp-row sp-side-row">
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-          </div>
-          <div className="sp-row sp-content-row">
-            <h1>Coming Soon</h1>
-            <h2>Designer Shoes that you dream of for incredible prices</h2>
-            <h1 className="sp-title"><em>Little</em> Blue Shoe</h1>
-          </div>
-          <div className="sp-row sp-side-row">
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-          </div>
-          <div className="sp-row">
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-            <span><img src="./images/yeoman.png"/></span>
-          </div>
-          </div></div>
-        </ScaleModal>
-      </div>
+      <div className="center col-xs-12"><ScaleModal ref="modal">
+          <div className="sp-content space">
+            <div className="sp-row">
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+            </div>
+            <div className="sp-row sp-side-row">
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+            </div>
+            <div className="sp-row sp-content-row">
+              <h1>!!!!!!!!!!!!!!!!</h1>
+            </div>
+            <div className="sp-row sp-side-row">
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+            </div>
+            <div className="sp-row">
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+              <span><img src="./images/yeoman.png"/></span>
+            </div>
+        </div>
+      </ScaleModal></div>
     );
   }
 }
