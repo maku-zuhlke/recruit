e5042ce8952c94d88cc569967561f621f3a53c47
lewa <lewa@zuhlke.com>
2016-08-03 14:17:16 +0100

    Referencing images during testing to prevent 'Image not found 404' warnings

--- karma.conf.js
+++ karma.conf.js
@@ -8,11 +8,10 @@ module.exports = function(config) {
     basePath: '',
     browsers: [ 'PhantomJS' ],
     files: [
-      'test/loadtests.js'
+      'test/loadtests.js',
+      {pattern: 'src/images/*.jpg', watched: false, included: false, served: true},
+      {pattern: 'src/images/*.png', watched: false, included: false, served: true}
     ],
-    proxies: {
-      '/images/': 'http://localhost:8000/images/'
-    },
     port: 8000,
     captureTimeout: 60000,
     frameworks: [ 'mocha', 'chai' ],
@@ -34,6 +33,9 @@ module.exports = function(config) {
         { type: 'html' },
         { type: 'text' }
       ]
+    },
+    proxies: {
+      '/images/': 'http://localhost:8000/base/src/images'
     }
   });
 };
