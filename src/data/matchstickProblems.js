73104a6367837db01bdf0707e9d942d9fdbda45a
lewa <lewa@zuhlke.com>
2016-08-05 17:34:08 +0100

    New matchstick puzzles

--- src/data/matchstickProblems.js
+++ src/data/matchstickProblems.js
@@ -1,7 +1,6 @@
 /**
  * Created by lewa on 03/08/2016.
  */
-
 export const m1 = {
   numbers :[[1,1,0,1,1,0,1],[0,1,1,0,1,1,1],[1,1,1,1,1,1,1]],
   operation: [1,0,0],
@@ -55,5 +54,37 @@ export const m7 = {
   operation: [1,0,0],
   moves: 2,
   correctPositions: {numbers: [[1,1,1,1,1,0,1],[1,1,1,1,1,1,0],[1,1,1,1,1,0,1]], operation: [1,0,1]},
+  win: false
+};
+
+export const m8 = {
+  numbers :[[1,1,1,1,1,0,1],[1,1,1,1,1,1,1],[1,1,0,1,1,0,1]],
+  operation: [1,0,0],
+  moves: 1,
+  correctPositions: {numbers: [[1,1,1,1,1,0,1],[1,1,1,1,1,1,0],[1,1,1,1,1,0,1]], operation: [1,0,0]},
+  win: false
+};
+
+export const m9 = {
+  numbers :[[1,1,1,1,1,1,0],[0,1,1,1,1,0,1],[0,1,1,0,1,1,1]],
+  operation: [1,0,1],
+  moves: 1,
+  correctPositions: {numbers: [[1,1,1,1,1,1,0],[0,1,1,1,1,0,1],[0,1,1,1,1,0,1]], operation: [1,0,1]},
+  win: false
+};
+
+export const m10 = {
+  numbers :[[1,1,1,1,1,1,0],[1,1,1,1,1,0,1],[0,1,1,1,1,0,1]],
+  operation: [1,0,0],
+  moves: 1,
+  correctPositions: {numbers: [[1,1,1,1,1,1,0],[0,1,1,1,1,0,1],[0,1,1,1,1,0,1]], operation: [1,0,1]},
+  win: false
+};
+
+export const m11 = {
+  numbers :[[0,1,1,0,1,1,1],[0,1,1,1,0,0,0],[1,1,1,1,1,1,0]],
+  operation: [1,0,1],
+  moves: 1,
+  correctPositions: {numbers: [[0,1,1,0,1,1,1],[0,1,1,1,0,0,0],[1,1,1,1,1,0,1]], operation: [1,0,1]},
   win: false
 };
