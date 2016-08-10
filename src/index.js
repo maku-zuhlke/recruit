428239e2c04d1cc9c3696833c5beaa9083173fcd
lewa <lewa@zuhlke.com>
2016-08-10 16:53:53 +0100

    react-router update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
 import { Router, Route, browserHistory } from 'react-router';
