ab96853ab1a9b65f5698c94334e848f2e0b6193b
lewa <lewa@zuhlke.com>
2016-08-18 14:03:21 +0100

    Initial page link

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,7 +14,7 @@ class AppComponent extends Component {
     return (
       <div className="index">
         <div className="row logoRow">
-          <div className="logo col-xs-3 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-2 col-lg-3 col-lg-offset-2"><span><img src="images/logo-zuhlke.png"/></span></div>
+          <div className="logo col-xs-3 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-2 col-lg-3 col-lg-offset-2"><span><a href="/"><img src="images/logo-zuhlke.png"/></a></span></div>
         </div>
         {this.props.children}
       </div>
