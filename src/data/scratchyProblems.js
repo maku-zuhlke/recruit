50fac36e6808a3397bd2b06bb1f3219cfe8a8687
lewa <lewa@zuhlke.com>
2016-08-18 17:57:54 +0100

    New coding challenges

--- src/data/scratchyProblems.js
+++ src/data/scratchyProblems.js
@@ -161,7 +161,7 @@ export const insertionSort = {
 };
 
 export const palindromes = {
-  instruction: codeBlocksInstruction + 'build method that checks if a given word is a palindrome',
+  instruction: codeBlocksInstruction + 'build a method that checks if a given word is a palindrome',
   correctOrder: [3, 5, 7, 2, 6, 1, 4],
   blocks: [
     {
@@ -191,5 +191,186 @@ export const palindromes = {
     {
       text: 'if (size >= 2) {',
       id: 7
+    }]
+};
+
+export const anagrams = {
+  instruction: codeBlocksInstruction + 'build a method that checks if two words are anagram of each other',
+  correctOrder: [5, 8, 6, 1, 10, 2, 11, 9, 3, 4, 7],
+  blocks: [
+    {
+      text: 'int index;\nchar[] chars = word.toCharArray();',
+      id: 1
+    },
+    {
+      text: 'index = otherWord.indexOf(character);',
+      id: 2
+    },
+    {
+      text: '} else {',
+      id: 3
+    },
+    {
+      text: 'return false;}}',
+      id: 4
+    },
+    {
+      text: 'public static boolean isAnagram(String word, String otherWord) {',
+      id: 5
+    },
+    {
+      text: 'return false;}',
+      id: 6
+    },
+    {
+      text: 'return otherWord.isEmpty();}',
+      id: 7
+    },
+    {
+      text: 'if (word.length() != otherWord.length()) {',
+      id: 8
+    },
+    {
+      text: 'otherWord = otherWord.substring(0, index) + otherWord.substring(index + 1, otherWord.length());',
+      id: 9
+    },
+    {
+      text: 'for (char character : chars) {',
+      id: 10
+    },
+    {
+      text: 'if (index >= 0) {',
+      id: 11
+    }]
+};
+
+export const secondHighest = {
+  instruction: codeBlocksInstruction + 'build a method prints the second highest number in an array',
+  correctOrder: [3, 8, 2, 6, 5, 9, 1, 7, 4],
+  blocks: [
+    {
+      text: '} else if (n > secondMax) {',
+      id: 1
+    },
+    {
+      text: 'for (int n : numbers) {',
+      id: 2
+    },
+    {
+      text: 'public static void secondHighest(int[] numbers) {',
+      id: 3
+    },
+    {
+      text: 'System.out.println(secondMax);}',
+      id: 4
+    },
+    {
+      text: 'secondMax = max;',
+      id: 5
+    },
+    {
+      text: 'if (n > max) {',
+      id: 6
+    },
+    {
+      text: 'secondMax = n;}}',
+      id: 7
+    },
+    {
+      text: 'int max = Integer.MIN_VALUE;\nint secondMax = Integer.MIN_VALUE;',
+      id: 8
+    },
+    {
+      text: 'max = n;',
+      id: 9
+    }]
+};
+
+export const sumOfLargestAndSmallest = {
+  instruction: codeBlocksInstruction + 'build a method prints the sum of the largest and smallest numbers in an array',
+  correctOrder: [4, 8, 1, 6, 3, 5, 2, 7],
+  blocks: [
+    {
+      text: 'for (int n : numbers) {',
+      id: 1
+    },
+    {
+      text: 'smallest = n;}}',
+      id: 2
+    },
+    {
+      text: 'largest = n;}',
+      id: 3
+    },
+    {
+      text: 'public static void sumOfLargestAndSmallest(int[] numbers) {',
+      id: 4
+    },
+    {
+      text: 'if (n < smallest) {',
+      id: 5
+    },
+    {
+      text: 'if (n > largest) {',
+      id: 6
+    },
+    {
+      text: 'System.out.println(largest + smallest);}',
+      id: 7
+    },
+    {
+      text: 'int largest = Integer.MIN_VALUE;\nint smallest = Integer.MAX_VALUE;',
+      id: 8
+    }]
+};
+
+
+export const binarySearch = {
+  instruction: codeBlocksInstruction + 'build the binary search method',
+  correctOrder: [5, 3, 7, 1, 9, 4, 10, 2, 6, 11, 8],
+  blocks: [
+    {
+      text: 'if (last == 0) {',
+      id: 1
+    },
+    {
+      text: '} else if (numbers[middle] < key) {',
+      id: 2
+    },
+    {
+      text: 'int first = 0;\nint last = numbers.length;',
+      id: 3
+    },
+    {
+      text: '} else if (numbers[middle] == key) {',
+      id: 4
+    },
+    {
+      text: 'public static int binarySearch(int[] numbers, int key) {',
+      id: 5
+    },
+    {
+      text: 'return binarySearch(Arrays.copyOfRange(numbers, middle + 1, last), key);',
+      id: 6
+    },
+    {
+      text: 'int middle = last/2;',
+      id: 7
+    },
+    {
+      text: 'return binarySearch(Arrays.copyOfRange(numbers, first, middle), key);}}',
+      id: 8
+    },
+    {
+      text: 'return -1;',
+      id: 9
+    },
+    {
+      text: 'return middle;',
+      id: 10
+    },
+    {
+      text: '} else {',
+      id: 11
     }]
 };
