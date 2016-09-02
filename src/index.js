f2e429a6a6651986ccac7f92ce829a16e4184cfe
Lachezar Kozhuharov <lachezar.kozhuharov@zuhlke.com>
2016-09-02 12:20:03 +0100

    Use hashHistory for routing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
-import { Router, Route, browserHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import App from './containers/App';
 import BlockList from './components/BlockChallenge/BlockList';
 import MatchstickPuzzle from './components/MatchPuzzle/MatchstickPuzzle';
@@ -10,7 +10,7 @@ import Menu from './components/Menu';
 
 const store = configureStore();
 const routes =
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route component={App}>
       <Route path="/" component={Menu} />
       <Route path="/coding" component={BlockList} />
